9ae5ef6ab0055e9fc55b171f082820cfcb486656
Riccardo Massidda <contact@rmassidda.it>
2022-05-07 12:53:16 +0200

    Make last by default

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build/index.html:
 	mkdir -p build
-	python src/html_generator.py archive/may_2022.md > build/index.mjml
+	python src/html_generator.py > build/index.mjml
 	mjml build/index.mjml > build/index.html
 	rm build/index.mjml
+
+clean:
+	rm -rf build
--- src/html_generator.py
+++ src/html_generator.py
@@ -1,11 +1,32 @@
-import sys
 import markdown
+import os
+import sys
+from datetime import datetime as dt
 
-with open('src/template.mjml', 'r') as f:
-    template = f.read()
+def render_mail(mail_path: str):
+    with open('src/template.mjml', 'r') as f:
+        template = f.read()
 
-with open(sys.argv[1], 'r') as f:
-    text = f.read()
+    with open(mail_path, 'r') as f:
+        text = f.read()
 
-mail = template.replace('%%%BODY%%%', markdown.markdown(text))
-print(mail)
+    return template.replace('%%%BODY%%%', markdown.markdown(text))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        mail_path = sys.argv[1]
+    else:
+        # Get the last mail in the
+        # archive folder. Mails are
+        # stored in the format
+        # "monthname_year.md"
+        mails = os.listdir('archive')
+        mails = sorted(
+            mails,
+            key=lambda x: dt.strptime(x, '%B_%Y.md'),
+            reverse=True
+        )
+        mail_path = f'archive/{mails[0]}'
+
+    print(render_mail(mail_path))
