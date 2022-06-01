69d81aab66ca650b583f29cb7a9f9059bd3df9e2
Jacopo <jacopomassa97@gmail.com>
2022-06-01 12:24:17 +0200

    Update html after fix

--- src/render_all.sh
+++ src/render_all.sh
@@ -4,10 +4,10 @@
 for file in archive/*.md; do
   # Get the filename without the extension
   filename=$(basename "$file" .md)
-  python src/mjml_generator.py "$file" >> "www/newsletter/$filename.mjml"
+  python3 src/mjml_generator.py "$file" >> "www/newsletter/$filename.mjml"
   mjml "www/newsletter/$filename.mjml" > "www/newsletter/$filename.html"
   rm "www/newsletter/$filename.mjml"
 done
 
 # Build index.html
-python src/index_generator.py > "www/newsletter/index.html"
+python3 src/index_generator.py > "www/newsletter/index.html"
