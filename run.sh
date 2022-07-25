cbc228c7f31ed6b95bf468baa03c61b81f2724ee
Riccardo Massidda <contact@rmassidda.it>
2022-07-25 07:13:43 +0200

    Do not track temporary files

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 python3 src/downloader.py >> log &&
-python3 src/render.py events.csv >> log &&
+python3 src/render.py -d 05/02/2022 events.csv >> log &&
 pug --doctype html --pretty layout/index.pug --out . &&
 mv index.html www/index.html &&
+rm layout/footer.pug layout/next.pug layout/upcoming.pug &&
 date >> log
