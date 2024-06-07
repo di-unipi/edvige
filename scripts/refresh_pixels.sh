27c7de2b46de9adf4f2696c1b23aa73624b1f5e0
Riccardo Massidda <riccardo.massidda@phd.unipi.it>
2024-06-07 19:26:36 +0200

    Move refreshing scripts

--- scripts/refresh_pixels.sh
+++ scripts/refresh_pixels.sh
@@ -5,17 +5,17 @@
 export DISPLAY=":0.0"
 
 # Get path of script
-SCRIPT_PATH=$(dirname $(realpath $0))
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 # Create log file
 LOGFILE="$SCRIPT_PATH/refresh_pixels.log"
 
 # Logging function to file ([YYYY-MM-DD HH:MM:SS] $1)
 function log {
-  echo [`date +"%Y-%m-%d %H:%M:%S"`] $1 >> "$LOGFILE"
+  echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1" >> "$LOGFILE"
 }
 
 # Get the window ID
-WID=`xdotool search --name "Mozilla Firefox" | head -1`
+WID=$(xdotool search --name "Mozilla Firefox" | head -1)
 
 # Check that WID is a number
 if ! [[ $WID =~ ^[0-9]+$ ]]; then
@@ -25,7 +25,7 @@ fi
 
 # Refresh Firefox
 log "Firefox has WID $WID"
-xdotool windowactivate $WID
+xdotool windowactivate "$WID"
 xdotool key ctrl+t
 log "Opened new tab"
 sleep 1
