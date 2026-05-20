72049d432133eb85f63c24170240ff534102db84
Jacopo Massa <jacopomassa97@gmail.com>
2026-05-20 16:33:32 +0200

    test: Add debug script

---
+++ scripts/calendar_debug.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -e
+
+# Log
+mkdir -p logs/
+echo "Start $(date)" >> logs/calendar.log
+
+# Render the pug files
+python3 scripts/render.py data/debug.ics >> logs/calendar.log
+
+# Check if static/assets/locandina.png exists
+if [ -f static/assets/locandina.png ]; then
+    node_modules/.bin/pug --doctype html --pretty layout/index_locandina.pug --out .
+    mv index_locandina.html index.html
+else
+    node_modules/.bin/pug --doctype html --pretty layout/index.pug --out .
+fi
+
+# Remove temporary pug files
+rm -f layout/footer.pug
+rm -f layout/events.pug
+
+# Move to folder
+mkdir -p www
+cp -r static/* www/
+mv index.html www/index.html
+
+echo "End $(date)" >> logs/calendar.log
